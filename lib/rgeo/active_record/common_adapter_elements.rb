219d2aff325deec1b7a6435295c27c67866f24b3
Tee Parham <tee@neighborland.com>
2015-02-07 13:54:21 -0700

    Remove GeoSchemaDumper hack
    * Remove support for “spatial: true” as an index option
    * Instead, adapters should use the standard AR way to specify a spatial
    index. For example: “using: :gist”

--- lib/rgeo/active_record/common_adapter_elements.rb
+++ lib/rgeo/active_record/common_adapter_elements.rb
@@ -91,41 +91,6 @@ module RGeo
 
     ::ActiveRecord::ConnectionAdapters::Table.send :include, GeoConnectionAdapters
 
-
-    # Hack schema dumper to output spatial index flag
-    module GeoSchemaDumper
-      def self.included(base)
-        base.class_eval do
-          alias_method :indexes_without_rgeo, :indexes
-          alias_method :indexes, :indexes_with_rgeo
-        end
-      end
-
-      private
-
-      def indexes_with_rgeo(table, stream)
-        indexes = @connection.indexes(table)
-        if indexes.any?
-          add_index_statements = indexes.map do |index|
-            statement = [
-                "add_index #{index.table.inspect}",
-                index.columns.inspect,
-                "name: #{index.name.inspect}",
-              ]
-            statement << 'unique: true' if index.unique
-            statement << 'spatial: true' if index.respond_to?(:spatial) && index.spatial
-            index_lengths = (index.lengths || []).compact
-            statement << ("length: #{::Hash[*index.columns.zip(index.lengths).flatten].inspect}") if index_lengths.any?
-            "  #{statement.join(', ')}"
-          end
-          stream.puts add_index_statements.sort.join("\n")
-          stream.puts
-        end
-      end
-    end
-
-    ::ActiveRecord::SchemaDumper.send :include, GeoSchemaDumper
-
     # :startdoc:
   end
 end
