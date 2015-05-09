b1da5cb22248af7dedcaf0a79061af2cf3b4d6fc
Tee Parham <tee@neighborland.com>
2015-05-09 16:15:50 -0600

    Add SpatialFactoryStore
    This is a replacement for RGeoFactorySettings et al

---
+++ test/spatial_factory_store_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class SpatialFactoryStoreTest < Minitest::Test
+  def test_default
+    store.default = nil
+    assert RGeo::Geos::CAPIFactory === store.default
+  end
+
+  def test_set_default
+    store.clear
+    default_factory = Object.new
+    store.default = default_factory
+    assert_equal default_factory, store.default
+  end
+
+  def test_register
+    store.clear
+    default_factory = Object.new
+    store.default = default_factory
+
+    point_factory = Object.new
+    store.register point_factory, geo_type: "point", srid: 4326
+    assert_equal point_factory, store.factory(geo_type: "point", srid: 4326)
+    assert_equal 1, store.registry.size
+    assert_equal point_factory, store.factory(geo_type: "point", srid: 4326)
+    assert_equal 1, store.registry.size
+
+    polygon_factory = Object.new
+    store.register polygon_factory, geo_type: "polygon"
+    assert_equal polygon_factory, store.factory(geo_type: "polygon")
+    assert_equal 2, store.registry.size
+
+    z_point_factory = Object.new
+    store.register z_point_factory, geo_type: "point", has_z: true
+    assert_equal z_point_factory, store.factory(geo_type: "point", has_z: true)
+
+    assert_equal default_factory, store.factory(geo_type: "linestring")
+  end
+
+  private
+
+  def store
+    RGeo::ActiveRecord::SpatialFactoryStore.instance
+  end
+end
