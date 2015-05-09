d3e2c6515459b4c28719a6f53ddb8e3729cc614b
Tee Parham <tee@neighborland.com>
2015-05-09 16:13:38 -0600

    Remove RGeoFactorySettings

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -8,36 +8,6 @@ class BasicTest < Minitest::Test # :nodoc:
     assert RGeo::ActiveRecord::VERSION
   end
 
-  def test_default_factory_generator
-    MyTable.rgeo_factory_generator = nil
-    factory = MyTable.rgeo_factory_for_column(:hello).call(has_z_coordinate: true, srid: 4326)
-    assert_equal true, factory.property(:has_z_coordinate)
-    assert_equal true, factory.property(:is_cartesian)
-    assert_nil factory.property(:is_geographic)
-    assert_equal 4326, factory.srid
-  end
-
-  def test_set_factory_generator
-    MyTable.rgeo_factory_generator = RGeo::Geographic.method(:spherical_factory)
-    factory = MyTable.rgeo_factory_for_column(:hello, has_z_coordinate: true, srid: 4326)
-    assert_equal true, factory.property(:has_z_coordinate)
-    assert_equal true, factory.property(:is_geographic)
-    assert_nil factory.property(:is_cartesian)
-    assert_equal false, factory.has_projection?
-    assert_equal 4326, factory.srid
-  end
-
-  def test_specific_factory_for_column
-    MyTable.rgeo_factory_generator = nil
-    MyTable.set_rgeo_factory_for_column(:foo, RGeo::Geographic.simple_mercator_factory(has_z_coordinate: true))
-    factory = MyTable.rgeo_factory_for_column(:foo)
-    assert_equal true, factory.property(:has_z_coordinate)
-    assert_equal true, factory.property(:is_geographic)
-    assert_nil factory.property(:is_cartesian)
-    assert_equal true, factory.has_projection?
-    assert_equal 4326, factory.srid
-  end
-
   def test_default_as_json_wkt
     RGeo::ActiveRecord::GeometryMixin.set_json_generator(nil)
     factory = RGeo::Cartesian.preferred_factory
