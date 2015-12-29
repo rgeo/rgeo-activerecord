0486d4ee42c169b2ccaaa7be0497ceb2828c1ee7
Tee Parham <tee@neighborland.com>
2015-12-29 12:04:17 -0700

    Fix #geometric_type_from_name for geometrycollection
    * The regex matches geometry. Move geometry after geometrycollection.
    * Sort types alphabetically (with the exception above).

--- lib/rgeo/active_record/common_adapter_elements.rb
+++ lib/rgeo/active_record/common_adapter_elements.rb
@@ -6,15 +6,15 @@ module RGeo
     # Some default column constructors specifications for most spatial
     # databases. Individual adapters may add to or override this list.
     DEFAULT_SPATIAL_COLUMN_CONSTRUCTORS = {
-      spatial:             { type: "geometry" }.freeze,
-      geometry:            {}.freeze,
-      point:               {}.freeze,
-      line_string:         {}.freeze,
-      polygon:             {}.freeze,
       geometry_collection: {}.freeze,
+      geometry:            {}.freeze,
+      line_string:         {}.freeze,
       multi_line_string:   {}.freeze,
       multi_point:         {}.freeze,
       multi_polygon:       {}.freeze,
+      point:               {}.freeze,
+      polygon:             {}.freeze,
+      spatial:             { type: "geometry" }.freeze,
     }.freeze
 
     # Index definition struct with a spatial flag field.
@@ -25,14 +25,14 @@ module RGeo
 
     def self.geometric_type_from_name(name)
       case name.to_s
-      when /^geometry/i then Feature::Geometry
-      when /^point/i then Feature::Point
-      when /^linestring/i then Feature::LineString
-      when /^polygon/i then Feature::Polygon
       when /^geometrycollection/i then Feature::GeometryCollection
-      when /^multipoint/i then Feature::MultiPoint
+      when /^geometry/i then Feature::Geometry
+      when /^linestring/i then Feature::LineString
       when /^multilinestring/i then Feature::MultiLineString
+      when /^multipoint/i then Feature::MultiPoint
       when /^multipolygon/i then Feature::MultiPolygon
+      when /^point/i then Feature::Point
+      when /^polygon/i then Feature::Polygon
       end
     end
   end
