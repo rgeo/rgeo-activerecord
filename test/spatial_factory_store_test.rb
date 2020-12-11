97fa8e8216b5d56a343669014ef40346394aa5a1
Keith Doggett <keith.doggett887@gmail.com>
2020-12-11 15:36:22 -0500

    Changed the SpatialFactoryStore to match the input attrs with the most similar factory instead of an exact match

--- test/spatial_factory_store_test.rb
+++ test/spatial_factory_store_test.rb
@@ -39,6 +39,57 @@ class SpatialFactoryStoreTest < Minitest::Test
     assert_equal default_factory, store.factory(geo_type: "linestring")
   end
 
+  def test_register_filter_attrs
+    store.clear
+    factory = Object.new
+    store.register(factory, { geo_type: "point", my_custom_field: "data" })
+
+    assert_equal(store.registry.first.attrs, { geo_type: "point" })
+  end
+
+  def test_fetch_factory
+    store.clear
+    default_factory = Object.new
+    store.default = default_factory
+
+    # test fallback
+    geom_factory = Object.new
+    store.register(geom_factory, { sql_type: "geometry" })
+    assert_equal(geom_factory, store.factory({ sql_type: "geometry", srid: 3857 }))
+
+    # test exact match
+    geom_merc_factory = Object.new
+    store.register(geom_merc_factory, { sql_type: "geometry", srid: 3857 })
+    assert_equal(geom_merc_factory, store.factory({ sql_type: "geometry", srid: 3857 }))
+
+    # test mismatched params
+    assert_equal(default_factory, store.factory({ sql_type: "geography", srid: 3857 }))
+  end
+
+  def test_fetch_factory_specificity
+    store.clear
+    default_factory = Object.new
+    store.default = default_factory
+
+    geom_factory = Object.new
+    store.register(geom_factory, { sql_type: "geometry", srid: 3857, geo_type: "point" })
+    assert_equal(default_factory, store.factory({ sql_type: "geometry", srid: 3857 }))
+  end
+
+  def test_fetch_factory_order
+    store.clear
+    default_factory = Object.new
+    store.default = default_factory
+
+    fac1 = Object.new
+    store.register(fac1, { sql_type: "geometry" })
+
+    fac2 = Object.new
+    store.register(fac2, { sql_type: "geometry" })
+
+    assert_equal(fac1, store.factory({ sql_type: "geometry" }))
+  end
+
   private
 
   def store
